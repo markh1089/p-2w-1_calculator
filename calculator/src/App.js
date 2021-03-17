6660f49f59a85b5b3a48d7084b5e9f366c9bb700
markh1089 <75758620+markh1089@users.noreply.github.com>
2021-03-17 20:42:18 +0000

    first commit to branch

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+//import Calculator from './Calculator.js';
+//import Keypad from './Keypad.js';
+//import Screen from './Screen.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      screen:""
+    }
+  }
+
+  buttonPress(){
+    console.log('1');
+    
+  }
+
+
+  render() {
+    return(
+      <>
+      <h1>Calculator App</h1>
+      <p>This is the calculator screen {this.state.count}</p>
+      <button onClick={() => this.buttonPress()}>1</button>
+      <button onClick={() => this.buttonPress()}>2</button>
+      <button onClick={() => this.buttonPress()}>3</button>
+      <button onClick={() => this.buttonPress()}>4</button>
+      <button onClick={() => this.buttonPress()}>5</button>
+      <button onClick={() => this.buttonPress()}>6</button>
+      <button onClick={() => this.buttonPress()}>7</button>
+      <button onClick={() => this.buttonPress()}>8</button>
+      <button onClick={() => this.buttonPress()}>9</button>
+      <button onClick={() => this.buttonPress()}>clear</button>
+      
+      </>
+    )
+  }
 }
 
 export default App;
